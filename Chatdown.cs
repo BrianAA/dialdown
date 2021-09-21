bd366fdb1297f2994c5021c073ec248fb8496980
BrianAA <brianalfaro@thoughttolife.com>
2021-09-20 21:57:30 -0400

    Chatdown refactor
    Documentation updated,
    Code refactor.

--- Chatdown.cs
+++ Chatdown.cs
@@ -8,30 +8,31 @@ public class Chatdown : MonoBehaviour
 {
     public TextAsset convoFile; //file to read for testing
     private string[] reader; //String reader
-    int currentRow = 0; //current line being read
-    int prevRowIndex = -1;//inital value is not positive
+    public int currentRow = 0; //current line being read
+    public int prevRowIndex = -1;//inital value is not positive
     string ReadText; //Text that is read by string reader
-    public string currentQuestion = "";
     public List<int> currentOptions = new List<int>();//stores current options and what row number they are on
     int currentDepth = 0;//Current thread to focus
-    [System.Serializable]
-    public enum stateOfDialogue { awaitingReponse, buildingOptions, chatting, jumping,endOfDialogue}; //states
-    bool endOfThread = false; //sets if it is an end of a thread
-    public stateOfDialogue dialogueState = stateOfDialogue.chatting; //dialuge state
 
-    enum actionType { isEvent, isEmotion, isFont, isSpeed, isJump, noAction }; //nested action types in string
+    [System.Serializable]
+    public enum stateOfDialogue {ready, awaitingReponse, buildingOptions, endOfDialogue, awaitingEvent }; //states
+    bool containsEnd = false;//if current thread has an End;
+    public stateOfDialogue dialogueState = stateOfDialogue.ready; //dialuge state
+
+    enum actionType { isEvent, isEmotion, isFont, isSpeed, isVariable, noAction }; //nested action types in string
 
     //Cache regex for performance
     private static Regex regexOptions = new Regex(" [0-9]. ", RegexOptions.Compiled);
     private static Regex regexQuestion = new Regex("## ", RegexOptions.Compiled);
     private static Regex regexPlayerName = new Regex("<character.player>", RegexOptions.Compiled);
     private static Regex regexFormater = new Regex("- ", RegexOptions.Compiled);
-    private static Regex regexEvent = new Regex("<event.", RegexOptions.Compiled);
-    private static Regex regexEmotion = new Regex("<emotion.", RegexOptions.Compiled);
-    private static Regex regexSpeed = new Regex("<speed.", RegexOptions.Compiled);
-    private static Regex regexFont = new Regex("<font.", RegexOptions.Compiled);
-    private static Regex regexJump = new Regex("<jump.", RegexOptions.Compiled);
-    private static Regex selectNumber = new Regex("[0-9]", RegexOptions.Compiled);
+    private static Regex regexEvent = new Regex("<event.*?>", RegexOptions.Compiled);
+    private static Regex regexEmotion = new Regex("<emotion.*?>", RegexOptions.Compiled);
+    private static Regex regexSpeed = new Regex("<speed.*?>", RegexOptions.Compiled);
+    private static Regex regexFont = new Regex("<font.*?", RegexOptions.Compiled);
+    private static Regex regexJump = new Regex("<jump.*?>", RegexOptions.Compiled);
+    private static Regex regexVariable = new Regex("<(variable.*?)>", RegexOptions.Compiled);
+
 
     ////Test script performance
     //System.Diagnostics.Stopwatch Stopwatch = new System.Diagnostics.Stopwatch();
@@ -41,7 +42,6 @@ public class Chatdown : MonoBehaviour
     {
         //splits the entire convo up
         reader = convoFile.text.Split(new[] { System.Environment.NewLine }, System.StringSplitOptions.None);
-        ReadConvo();
     }
     /// <summary>
     /// Takes a textasset and send to dialogue handler
@@ -65,35 +65,34 @@ public class Chatdown : MonoBehaviour
         if (ValidLine && currentRow != prevRowIndex)
         {
             ReadText = reader[currentRow];//gets the next line
-            prevRowIndex = currentRow;//Stores to ensure there are no infinite loops
+            prevRowIndex = currentRow;
             int lineDepth = Regex.Matches(ReadText, "- ").Count; //Checks depth of line
 
-            //Ensures string is correct depth
-            if (lineDepth == currentDepth && //If it has the same depth of current thread
-                dialogueState!=stateOfDialogue.endOfDialogue && //not the end of the dialogue
-                dialogueState != stateOfDialogue.awaitingReponse && //is not awaiting a response
-                dialogueState != stateOfDialogue.jumping) // Currently jumping to new line
+            if (lineDepth == currentDepth)
             {
                 string prepString = prepareLineofText(ReadText); //replaces string variables
 
-                if (regexQuestion.IsMatch(prepString)) //checks if a question
+                if (regexQuestion.IsMatch(prepString)&&dialogueState!=stateOfDialogue.buildingOptions) //checks if a question
                 {
                     prepString = regexQuestion.Replace(prepString, ""); //removes ##
                     dialogueState = stateOfDialogue.buildingOptions; //sets state to build options
-                    currentQuestion=currentQuestion == "" ? currentQuestion = prepString : currentQuestion; //stores questions and looks for options
+                    ExecuteString(prepString);
                 }
                 else if (regexOptions.IsMatch(prepString))//if its not a question
                 {
                     prepString = regexOptions.Replace(prepString, ""); //removes number
                     currentOptions.Add(currentRow); //adds to option to display
                 }
+                else if (regexJump.IsMatch(prepString))//if its a jump
+                {
+                    HandleJump(prepString);
+                }
                 else //if just a regular message or other event, etc
                 {
                     //if currently building option and reaches a line that is not an option
                     if (dialogueState == stateOfDialogue.buildingOptions)
                     {
-                            dialogueState = stateOfDialogue.awaitingReponse; //set to awaiting the reponse show options
-                            ExecuteString(currentQuestion); //Display question
+                        dialogueState = stateOfDialogue.awaitingReponse; //set to awaiting the reponse show options
                     }
                     //Not an option or a question and not waiting for options just plain message
                     else
@@ -105,20 +104,23 @@ public class Chatdown : MonoBehaviour
                         }
                     }
                 }
-                //repeats method until reaches an end of a thread
-                if (!endOfThread)
-                {
-                    currentRow++;//proceed to next row
-                    ReadConvo();//repeat
-                }
             }
-            //handles lines not in the current thread
-            else
+            if (dialogueState == stateOfDialogue.buildingOptions)
             {
                 currentRow++;//proceed to next row
                 ReadConvo(); //repeat
             }
         }
+    }
+
+    //Handles going to the next line of text
+    public void processNextLine()
+    {
+        if (Ui_Dialogue.currentStatus == Ui_Dialogue.status.processing) return;//makes sure the text finish displaying
+        if (dialogueState == stateOfDialogue.awaitingReponse) return;
+        if (dialogueState == stateOfDialogue.endOfDialogue) return;
+        currentRow++;
+        ReadConvo();
     }
 
     /// <summary>
@@ -127,21 +129,32 @@ public class Chatdown : MonoBehaviour
     /// <param name="selectedOption"></param>
     public void SelectOption(int selectedOption)
     {
-        currentRow = currentOptions[selectedOption - 1] + 1;
+        // IF PROCESSING THE TEXT VIA UI STOP CHECK BEFORE ALLOWING USERS TO SELECT OPTION
+        // Example if(UiDialogue.isProcessing) return
+
+        if (selectedOption > currentOptions.Count) return;//makes sure the option is valid choice
+        currentRow = currentOptions[selectedOption - 1] + 1; //sets the current row to option
         currentOptions.Clear(); //Clear out options
-        currentQuestion = "";
         currentDepth++; //shifts to nested thread
-        endOfThread = false;
-        dialogueState = stateOfDialogue.chatting; //change from awaiting reponse to chatting
+        dialogueState = stateOfDialogue.ready; //change from awaiting reponse to chatting
+        ReadConvo(); //invoke convo
+    }
+    /// <summary>
+    /// When event completes call this method to continue conversation
+    /// </summary>
+    public void EventCompleted()
+    {
+        dialogueState = stateOfDialogue.ready;//event ready to go next line
+        currentRow++;//proceed to next row
         ReadConvo(); //invoke convo
     }
 
-    //Prepares string to be displayed (replaces location, names)
+    //Prepares string to be displayed (replaces location, names & variables)
     string prepareLineofText(string ReadText)
     {
         string prepString = regexFormater.Replace(ReadText, "");//Removes '- ' from text
         prepString = regexPlayerName.Replace(prepString, "Player");// Replaces with characters name
-
+        prepString = regexVariable.IsMatch(prepString) ? HandleVariable(prepString) : prepString;
         return prepString;
     }
 
@@ -149,56 +162,49 @@ public class Chatdown : MonoBehaviour
     void ExecuteString(string prepString)
     {
         string[] splitStrings = prepString.Split(' ');//Splits string into words
-        actionType isAction = actionType.noAction;
-        Debug.Log(prepString);
+        actionType inlineAction;
+        EventManager.TriggerEvent("NextLine");//clears ui Dialogue for the next line of text
+
+        //Loop through all the words
         for (int i = 0; i < splitStrings.Length; i++)
         {
             //checks action type
-            isAction = regexEvent.IsMatch(splitStrings[i]) ? actionType.isEvent :
-                regexEmotion.IsMatch(splitStrings[i]) ? actionType.isEmotion :
-                regexFont.IsMatch(splitStrings[i]) ? actionType.isFont :
-                regexSpeed.IsMatch(splitStrings[i]) ? actionType.isSpeed :
-                regexJump.IsMatch(splitStrings[i]) ? actionType.isJump : actionType.noAction;
+            inlineAction = regexEvent.IsMatch(splitStrings[i]) ? actionType.isEvent:
+                regexEmotion.IsMatch(splitStrings[i]) ? actionType.isEmotion:
+                regexFont.IsMatch(splitStrings[i]) ? actionType.isFont:
+                regexSpeed.IsMatch(splitStrings[i]) ? actionType.isSpeed:actionType.noAction;
+
             //Switch based on action type
-            switch (isAction)
+            switch (inlineAction)
             {
                 case actionType.isEvent:
                     HandleEvent(splitStrings[i]);
                     break;
                 case actionType.isEmotion:
-                    Debug.Log("Triggered Emotion");
+                    Debug.Log("Triggered Emotion"); //handle emotion animation
                     break;
                 case actionType.isFont:
-                    Debug.Log("Changed Font size");
+                    Debug.Log("Changed Font size"); //handle fontsize change on ui
                     break;
                 case actionType.isSpeed:
-                    Debug.Log("Changed speed of text");
-                    break;
-                case actionType.isJump:
-                    dialogueState = stateOfDialogue.jumping;
-                    MatchCollection matches = selectNumber.Matches(splitStrings[i]);
-                    string stringToParse = "";
-
-                    foreach (Match m in matches)
-                    {
-                        stringToParse = stringToParse + m.Value;
-                    }
-                    if (stringToParse != "")
-                    {
-                        Debug.Log("Jumping to line " + stringToParse);
-                        currentRow = int.Parse(stringToParse) - 1;
-                        currentDepth = setDepth(reader[currentRow]);
-                        dialogueState = stateOfDialogue.chatting;
-                        ReadConvo();
-                    }
+                    Debug.Log("Changed speed of text"); //handle speed of text in ui
                     break;
                 default:
+                    //If it just text send to UI dialouge
                     if (splitStrings[i] != "" && splitStrings[i] != " ")
                     {
-                        //Debug.Log(splitStrings[i]);
+                        Debug.Log("Handle text");//Handle text
                     };
                     break;
             }
+        }
+        if (containsEnd)//if its an end of dialogue its ready to close out
+        {
+            //Wait for dialouge to finish displaying before ending
+            // example while (UiDialogue.isprocessing) return
+            dialogueState = stateOfDialogue.endOfDialogue;//Sets dialogue to end close out
+            EventManager.TriggerEvent("EndDialogue");//Close Dialogue;
+            Debug.Log("Ended Dialogue");
         }
     }
 
@@ -210,22 +216,42 @@ public class Chatdown : MonoBehaviour
     void HandleEvent(string eventName)
     {
         //handles if the dialogue comes to an end
-        if(eventName=="<event.end>")
+        if (eventName == "<event.end>")
         {
-            Debug.Log("Ending Dialogue");
-            dialogueState = stateOfDialogue.endOfDialogue;//Sets dialogue to end close out
+            containsEnd = true;
         }
         else
         {
-            string splitString = eventName.Split('.')[1];
-            string methodToInvoke = splitString.Substring(0, splitString.Length - 1);
-            Debug.Log("Invoking " + methodToInvoke);
-
-
+            string splitString = eventName.Split('.')[1]; //split string after <event.
+            string methodToInvoke = splitString.Substring(0, splitString.Length - 1); //remove the trailing > from nameofEvent>
+            Debug.Log("Invoking " + methodToInvoke); // Invoke the event best to use a event manager
+            dialogueState = stateOfDialogue.awaitingEvent;
+            prevRowIndex = currentRow;
         }
     }
+
+    void HandleJump(string prepString)
+    {
+        string stringToParse = prepString.Split('.')[1].Substring(0, prepString.Split('.')[1].Length - 1);// returns everything after <jump. and subtracts last >
+        if (stringToParse != "")
+        {
+            Debug.Log("Jumping to line " + stringToParse);
+            currentRow = int.Parse(stringToParse) - 1;//Set the row to jump to as current
+            currentDepth = setDepth(reader[currentRow]);//get the depth it should be reading at
+            ReadConvo();
+        }
+    }
+
+    string HandleVariable(string stringToChange)
+    {
+        Debug.Log("Handling variable");
+        //Example string processString = regexVariable.Replace(stringToChange, "$200.00");
+        return processString;
+    }
+
     void Update()
     {
+        //Simulates controls
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             SelectOption(1);
@@ -238,5 +264,23 @@ public class Chatdown : MonoBehaviour
         {
             SelectOption(3);
         }
+
+        //Starts conversation
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReadConvo();
+        }
+        //Complete event
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            EventCompleted();
+        }
+        //next line to process
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            processNextLine();
+        }
     }
 }
+
+
--- README.md
+++ README.md
@@ -1,50 +1,102 @@
 # Chatdown
-A simple dialogue system for unity. It utilizes a simple text format with keys and events to feed information into a script to handle the entire conversation. The name comes from markdown as the formatting of the file uses markdown type keys only the inline actions and variables utilize `<>` to seperate from the the rest of the text.
+Chatdown is a dialogue system for unity3d. It uses a text file in a markdown style formatting and processes that markdown into a dialogue with questions, options, nested dialogue, variables, and more. It can be extended to suite your dialogue needs and tries unopinionated on how it is implemented ui. The text file is dependant highly on formatting, but very easy to learn. 
 
 ## Getting started
-If you like to use Chatdown simply drag and drop the main script `Chatdown.cs` to your asset folder (probably best in a script folder). In order to use it you will need to drag and drop it onto a game object. It will then take any TextAsset convert it to text split it up and handle the conversation based on the information of each line.
+Chatdown can be up and running with little time as possible. You can then spend time on designing a ui dialogue and refining the user experience.
+
+1. First copy or download the `Chatdown.cs` file into your unity asset folder
+2. You will then need to add it to any gameobject that make sense for your setup.
+3. To start using you can drag a textfile into the public textasset for testing but you will want to load text assets via `StartDialogue` method ideally.
+```
+    public void StartDialogue(TextAsset dialogueFile)
+    {
+        reader = dialogueFile.text.Split(new[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+        ReadConvo();
+    }
+```
+5. Next handle all the cases `ExecuteString()` fires. (emotion,events,speed,font)and you are ready to roll. 
+
 
 ## Writing out dialogue
 There are three dialogue types: `questions` `options` and `basic` and everything is controlled by `-` which will indicate the current depth and thread.
 
 ### Question and Options
-Questions and options are always paired together a question can never appear without options following it. Options also need to be more than one other wise it might as well not be presented to the user as an option. If you are to write a question the following format should be followed:
+Questions and options are always paired together a question should never appear without options following it unless using a loop type conversation. 
+
+##### Standard Question and option
 ```
 ## Type out your question
 1. Type your option
   - Nested Response add more '-' based on nested depth
 2. Type your option
   - Nested Response add more '-' based on nested depth
- ```
- 
- `##` Is the flag to let Chatdown know that this is a questions it will then be searching for the options looking for `[0-9]. ` the number and period. It will then build out the options and await a response before moving forward. 
- 
-#### Nested question
 ```
-## Type out your question
-1. Type your option
+##### Standard Question and option with loop
+```
+## How can I help you?
+1. I want to sell
   - Nested Response add more '-' based on nested depth
-  - ## Nested question
-  - 1. Nested option will go here
-    - Nested response
-    - <event.endThread>
-  - - Nested response when that nested question is selected
-  - ## Second Nested question
-  - 2. Nested option will go here
-    - Nested response
-    - <event.endThread>
-  - - Nested response when that nested question is selected
-2. Type your option
+  - <jump.7>
+2. I want to buy
   - Nested Response add more '-' based on nested depth
-  - <event.endThread>
+  - <jump.7>
+3. Uh nevermind..
+  - <event.end>
+## Anything else I can help you with?
+```
+ 
+ `##` is the flag to let Chatdown know that this line is a questions switch Chatdown dialogueState to `buildingOptions` This will allow Chatdown to loop through the entire text file looking for options. Options are flagged by a number, period and a space example `1. ` The order of the options will always be in the order they are from top to bottom regardless of the actual number.
+
+ 
+#### Nesting
+
+Nesting conversations are triggered when options and questions are used. A thread is indicated by `-` if there is none it is the current thread is 0. 
+
+##### Example of nesting threads
+```
+## How can I help you?
+1. I want to buy
+  - Great I got plenty to offer! 
+  - ## So what will it be for you today? <event.openShop>
+  - 1. 10 potions - <variable.potionValueBulk>
+  - - Great! here you go! <event.Sold.potionValueBulk>
+  - - <jump.1>
+  - 2. 1 magic spell <variable.magicSpellValue>
+  - - Great! here you go! <variable.magicSpellValue>
+  - - <jump.1>
+2. I want to sell
+  - Let see what you got! <event.openSellMode>
+  - ## That will be <variable.currentSaleTotal> sound good?
+  - 1. Sure! <event.sold>
+    - - <jump.1>
+  - 2. No thanks Ill keep my stuff <event.cancel>
+    - - <jump.1>
+3. I am okay thanks..
+  - <event.end>
  ```
+
+
 #### Basic text
+Basic text is treated per line. 
+
+##### One line of text
 ```
 A basic message
 ```
 
+##### 2 line of text
+```
+A basic message
+A another line because I made it on another line
+```
+
 ### Actions
-What are actions? Actions are anything you want them to be. Currently in Chatdown there are events, emotions, font size changes, speed of text, and ability to jump to lines. You are not required to utilize any of the actions written out and you can create your own simply create a Regex pattern using the format `"<custom."` as a cached regex pattern see variables above and add it to the [ExecuteString](https://github.com/BrianAA/chatdown/blob/60fe9b568be8bf64d2b51ee4e76d67b24e3c26a0/Chatdown.cs#L148-L187) method in the actiontype as well as the switch statement. 
+What are actions? Actions are anything you want them to be. Currently in Chatdown there are events, emotions, font size changes, speed of text as examples and can be deleted or modified. 
+
+Actions are flagged based on the regex pattern. You can create you own utilizing any regex pattern that isn't already used. 
+
+Actions can be anything really change the text speed, or maybe open and fire in game methods. Chat down works best if you have an event handler in your game that listens for event triggers. 
+
 
 #### Basic use of an action
 All actions are the same they will fire exactly where you place them the most important rule of them is that they should always have a space before and after the action. For example:
@@ -78,12 +130,22 @@ Wow look who it is <character.player>
 ```
 
 ### Replacing variables
-There is support to swap out variables for the text of your choice. If need things to be dynamic like the name of the character because it will vary from player to player then using variables will definitely be something you want to make use of. To use any variable you need to set up a regex pattern simialr to the event pattern using `<variable.` Currently there is support built in for `<character.playyer>` and will swap it out whenever it is used with the player name. 
+There is support to swap out variables for the text of your choice. If need things to be dynamic like the name of the character because it will vary from player to player then using variables will definitely be something you want to make use of. To use any variable you need to can add `<variable.yourvariable>` it will replace it with a global accessible variable you assign. Characters and are handle seperately but follow similar patterns `<characters.nameOfCharacter>` 
 
 #### Basic use of variables
 ```
 Hi there <character.player>!
 ```
+
+#### Use of variable that needs to be calculated programmatically 
+For variables that need to be calculated the `handleVariable` method will be where you want to handle variables and prepare the string prior to `Chatdown` passing it to `ExecuteString()`
+
+##### Example
+```
+<!-- This assumes getTotalAmount was calculated in an event or prior -->
+That will be <variables.getTotalAmount>!
+```
+
 ## Using VScode
 Not required but highly reccomended to utilize VScode snippets. They help write out dialogue a lot faster I simply pull up the snippet and it will drop inplace the thing that I need question with 2, 3, or 4 options etc. Highly recommend it! Write your own following the format or using [snippet generator](https://snippet-generator.app/)
 
